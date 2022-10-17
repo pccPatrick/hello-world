4ada2a9d04b53f9ca43c1edbc6c29b4e94419cd5
Patrick Foster <patrick.foster@pcc.edu>
2022-10-17 12:07:53 -0700

    Added comment header to source code file.

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -2,6 +2,13 @@ package edu.pcc.cis.helloworld;
 
 import java.util.Date;
 
+/**
+ * A simple hello world program.
+ *
+ * @author Patrick Foster
+ * @version 2022.10.17
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
